6b25656a6d7dcb5aa9be80ec00377bad71a258a0
M.ghalandari <m.ghalandari@ostfalia.de>
2023-05-04 20:41:13 +0200

    vote and user are done

--- app/controllers/vote.controller.js
+++ app/controllers/vote.controller.js
@@ -7,44 +7,47 @@ const Token = db.tokens;
 
 //Add a new vote to the poll
 const addVote = async (req, res) => {
-    const tokenValue = req.params.token;
-    const { owner, choice } = req.body;
-  
-    try {
-      const token = await Token.findOne({ where: { value: tokenValue } });
-  
-      if (!token) {
-        return res.status(404).json({ code: 404, message: 'Token not found' });
-      }
-  
-      const poll = await Poll.findByPk(token.poll_id);
-  
-      if (!poll) {
-        return res.status(404).json({ code: 404, message: 'Poll not found' });
-      }
-  
-      const user = await User.create({ name: owner.name });
-      const votePromises = choice.map(({ id, worst }) =>
-        Vote.create({
-          user_id: user.id,
-          poll_option_id: id,
-          poll_id: poll.id,
-          worst: worst || false,
-        })
-      );
-      const votes = await Promise.all(votePromises);
-  
-      res.status(200).json({
-        description: 'The result after creating a vote.',
-        edit: {
-          link: '/vote/edit/' + tokenValue,
-          value: tokenValue,
-        },
-      });
-    } catch (error) {
-        console.error('Error in addVote:', error);
-        res.status(405).json({ code: 405, message: 'Invalid input' });
+  const tokenValue = req.params.token;
+  const { owner, choice } = req.body;
+
+  try {
+    const token = await Token.findOne({ where: { value: tokenValue } });
+
+    if (!token) {
+      return res.status(404).json({ code: 404, message: 'Token not found' });
     }
+
+    const poll = await Poll.findByPk(token.poll_id);
+
+    if (!poll) {
+      return res.status(404).json({ code: 404, message: 'Poll not found' });
+    }
+
+    const user = await User.create({
+      name: owner.name
+    });
+
+    const votePromises = choice.map(({ id, worst }) =>
+      Vote.create({
+        user_id: user.id,
+        poll_option_id: id,
+        poll_id: poll.id,
+        worst: worst || false,
+      })
+    );
+    const votes = await Promise.all(votePromises);
+
+    res.status(200).json({
+      description: 'The result after creating a vote.',
+      edit: {
+        link: '/vote/edit/' + tokenValue,
+        value: tokenValue,
+      },
+    });
+  } catch (error) {
+    console.error('Error in addVote:', error);
+    res.status(405).json({ code: 405, message: 'Invalid input' });
+  }
 };
 
 //Find the vote of the token
--- app/models/vote.model.js
+++ app/models/vote.model.js
@@ -3,27 +3,6 @@ module.exports = (sequelize, DataTypes) => {
     worst: {
       type: DataTypes.BOOLEAN,
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'users',
-        key: 'id'
-      }
-    },
-    poll_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'polls',
-        key: 'id'
-      }
-    },
-    poll_option_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'poll_options',
-        key: 'id'
-      }
-    },
   });
   return Vote;
 };
