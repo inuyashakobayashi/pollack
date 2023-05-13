754c701756ebd5d63050c843e502c8264075cec9
M.ghalandari <m.ghalandari@ostfalia.de>
2023-05-13 19:26:40 +0200

    make Poll.js better design

--- client/src/pages/Poll.js
+++ client/src/pages/Poll.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import PollList from './PollList'
+import React from 'react';
+import { Container, Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import PollList from './PollList';
+
 function Poll() {
   return (
-    <Container>Everything about Poll
-        <br />
-        <Link to='/addPoll'>
-        <button >new poll</button>
-      </Link>
-      <hr/>
-
-      <Container>
-        <PollList />
-      </Container>
-
+    <Container className="mt-5">
+      <Card className="text-center">
+        <Card.Header as="h2">Everything about Poll</Card.Header>
+        <Card.Body>
+          <Link to='/addPoll'>
+            <Button variant="primary" size="lg">Create New Poll</Button>
+          </Link>
+          <hr/>
+          <Container className="mt-4">
+            <PollList />
+          </Container>
+        </Card.Body>
+      </Card>
     </Container>
   )
 }
 
-export default Poll
+export default Poll;
