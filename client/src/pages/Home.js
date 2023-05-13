1a4a0ae8f7e6fef55ad03670aa0fc2a6e39dd1ef
M.ghalandari <m.ghalandari@ostfalia.de>
2023-05-13 20:08:09 +0200

    make a better design for home and header components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-
-
+import { Button, Container, Card } from 'react-bootstrap';
 
 function Home() {
-
   return (
-    <div>
-      <h1>Home</h1>
-      <br/>
-
-      <Link to='/poll'>
-        <button >Poll</button>
-      </Link>
-
-    <br/> <br/>
-    <Link to='/vote'>
-        <button >Vote</button>
-      </Link>
-
-    </div>
-  )
+    <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+      <Card className="text-center" style={{ width: '30rem' }}>
+        <Card.Body>
+          <Card.Title className="mb-4"><h1>Welcome Home!</h1></Card.Title>
+          <Card.Text>
+            Participate in our community polls or cast your vote in an existing one.
+          </Card.Text>
+          <Link to="/poll">
+            <Button variant="primary" className="m-2">
+              Create a Poll
+            </Button>
+          </Link>
+          <Link to="/vote">
+            <Button variant="success" className="m-2">
+              Vote in a Poll
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
